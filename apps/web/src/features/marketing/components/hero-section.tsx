2949ddb52878cbad9328dbfcee076ccea06c9f34
Maged Ibrahim <magedibra@yahoo.com>
2026-05-20 19:19:32 +0300

    chore: improve hero-section styles

--- apps/web/src/features/marketing/components/hero-section.tsx
+++ apps/web/src/features/marketing/components/hero-section.tsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router'
 
 function HeroSection() {
   return (
-    <section className='flex py-4 items-center gap-3 text-center flex-col min-h-[calc(100vh-var(--header-height))]'>
-        <h3 className='text-2xl font-bold'>Navigate your career path with AI precision</h3>
-        <p>Build your personalized roadmap to your dream tech job with our AI-powered structured platform.</p>
-        <Button render={<Link to="/sign-up"/>}>
-            Start Free <ArrowRight />
-        </Button>
+    <section className='flex py-12 items-center text-center justify-between gap-3 flex-col min-h-[calc(100vh-var(--header-height))]'>
+          <div className="max-w-2xl space-y-4">
+            <h3 className='text-3xl font-bold'>Navigate your career path with AI precision</h3>
+            <p>Build your personalized roadmap to your dream tech job with our AI-powered structured platform.</p>
+            <Button render={<Link to="/sign-up"/>}>
+                Start Free <ArrowRight />
+            </Button>
+          </div>
+          {/* TODO: add in-app image */}
     </section>
   )
 }
