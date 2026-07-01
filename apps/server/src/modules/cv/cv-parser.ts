801b3ce84d672af4fe50c895d4220be3b193f102
Belal <belalemara798@gmail.com>
2026-07-01 08:08:39 +0300

    feat(cv): cancel the webhook and create an modified route for reciving data on the same route

--- apps/server/src/modules/cv/cv-parser.ts
+++ apps/server/src/modules/cv/cv-parser.ts
@@ -3,7 +3,6 @@ import { cv } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { env } from "@careergps/env/server";
 
-<<<<<<< HEAD
 export async function parseCVData(cvId: string, url: string): Promise<unknown> {
   try {
     // mark as parsing before sending
@@ -56,43 +55,6 @@ export async function parseCVData(cvId: string, url: string): Promise<unknown> {
 
   } catch (error) {
     console.error("parsing service failed:", error);
-=======
-export async function parseCVData(cvId: string, fileUrl: string) : Promise<unknown> {
-  try{
-    // mark as parsing before sending
-    await db
-    .update(cv)
-    .set({ status: "parsing" })
-    .where(eq(cv.id, cvId));
-
-    const parsingEndPoint = `${env.AI_TEAM_URL}/parse`;
-
-    const body = JSON.stringify({
-      cvId,
-      fileUrl,
-    });
-
-    const res = await fetch(parsingEndPoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body,
-    });
-
-    if (!res.ok) {
-      await db
-      .update(cv)
-      .set({ status: "failed" })
-      .where(eq(cv.id, cvId));
-      throw new Error(`AI Team rejected request: ${res.status}`);
-    }
-
-    return await res.json() ;
-    
-  }catch(error){
-    console.error("parsing service failed:" ,error);
->>>>>>> cc56ec0fffdf9ae0f78ecfeec939e7632c58e1ca
     await db
       .update(cv)
       .set({ status: "failed" })
@@ -100,12 +62,6 @@ export async function parseCVData(cvId: string, fileUrl: string) : Promise<unkno
     throw error;
   }
 
-<<<<<<< HEAD
-=======
-
-
-  // return parsedData ;
->>>>>>> cc56ec0fffdf9ae0f78ecfeec939e7632c58e1ca
 }
 
 export default parseCVData;
